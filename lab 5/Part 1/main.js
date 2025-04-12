89392f1ee2f0c6f241fb40bfc8da5620c22c5e56
KRISH0824 <krish.patel30@dcmail.ca>
2025-04-11 21:38:43 -0400

    Lab 5 part 1
    basics think like body and all

---
+++ lab 5/Part 1/main.js
@@ -0,0 +1,63 @@
+/*
+    Name: Krish Patel
+    Date: 10th April, 2025
+    Purpose: Assignment 5
+    File: main.js
+    Desc: Accessibility troubleshooting
+*/
+
+// functionality for showing/hiding the comments section
+
+const showHideBtn = document.querySelector('.show-hide');
+const commentWrapper = document.querySelector('.comment-wrapper');
+
+commentWrapper.style.display = 'none';
+
+showHideBtn.onclick = function() {
+  let isExpanded = showHideBtn.getAttribute('aria-expanded') === 'true';
+
+  if (!isExpanded) {
+    showHideBtn.textContent = 'Hide comments';
+    commentWrapper.style.display = 'block';
+    showHideBtn.setAttribute('aria-expanded', 'true');
+  } else {
+    showHideBtn.textContent = 'Show comments';
+    commentWrapper.style.display = 'none';
+    showHideBtn.setAttribute('aria-expanded', 'false');
+  }
+};
+
+
+// functionality for adding a new comment via the comments form
+
+const form = document.querySelector('.comment-form');
+const nameField = document.querySelector('#name');
+const commentField = document.querySelector('#comment');
+const list = document.querySelector('.comment-container');
+
+form.onsubmit = function(e) {
+  e.preventDefault();
+  submitComment();
+};
+
+function submitComment() {
+  const listItem = document.createElement('li');
+  const namePara = document.createElement('p');
+  const commentPara = document.createElement('p');
+  const nameValue = nameField.value;
+  const commentValue = commentField.value;
+
+  namePara.textContent = nameValue;
+  commentPara.textContent = commentValue;
+
+  list.appendChild(listItem);
+  listItem.appendChild(namePara);
+  listItem.appendChild(commentPara);
+
+  nameField.value = '';
+  commentField.value = '';
+}
+
+/* added trhe button */
+
+showHideBtn.setAttribute("aria-expanded", commentWrapper.style.display === 'block');
